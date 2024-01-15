6482eaa24352dbe4044777923ee6a4245b3513f7
Ray Burks <ray_burks@outlook.com>
2024-01-15 16:47:47 +0100

    addsbqueuetrigger

--- python/3.11/AzureFunctions/ServiceBusTopicTriggerSample/function_app.py
+++ python/3.11/AzureFunctions/ServiceBusTopicTriggerSample/function_app.py
@@ -3,7 +3,7 @@ import logging
 
 app = func.FunctionApp()
 
-@app.service_bus_topic_trigger(arg_name="azservicebus", subscription_name="mysubscription", topic_name="mysbtopic",
+@app.service_bus_topic_trigger(arg_name="azservicebus", subscription_name="demo_subscription", topic_name="demo",
                                connection="ServiceBusConnectionString") 
 def servicebus_topic_trigger(azservicebus: func.ServiceBusMessage):
     logging.info('Python ServiceBus Topic trigger processed a message: %s', azservicebus.get_body().decode('utf-8'))
